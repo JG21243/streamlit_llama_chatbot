ac2d4ba3ab6e0651cd1b3a4b3351563ee2e34857
Josh Glen <129418264+JG21243@users.noreply.github.com>
2023-09-01 15:44:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from llama_index import VectorStoreIndex, ServiceContext, Document
+from PyPDF2 import PdfFileReader, PdfFileWriter
 from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
