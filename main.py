be267da168f1e70c191aa3b3dce6f29eff97c37a
Josh Glen <129418264+JG21243@users.noreply.github.com>
2023-09-01 15:30:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def load_data():
         return index
 
 index = load_data()
-# chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features.")
+# chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True, system_prompt="You are an expert at answering user questions on legal subjects. You should answer specific questions based on information available to you. If you do not have the information, do not hallucinate, but tell the user you do not know the answer.")
 chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
